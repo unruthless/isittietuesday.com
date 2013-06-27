30eb5ad1cc97598145fb19a1ec5492f765f977ab
William Clark <william.a.clark@gmail.com>
2013-06-27 12:05:32 -0700

    Favicon now is programmatic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
+$(document).ready(function() {
+
+	if (isTodayTuesday()) {
+		$('body').attr('data-isTuesday','true');
+		$('.answer-text').text('Yep.');
+		changeFavicon('favicon_yes.ico');
+
+	} else {
+
+		$('body').attr('data-isTuesday','false');
+		$('.answer-text').text('Nope.');
+		changeFavicon('favicon_no.ico');
+
+	}
+});
+
 function isTodayTuesday() {
 
 	var date = new Date(),
@@ -8,17 +24,19 @@ function isTodayTuesday() {
 	return (day === 2);
 }
 
-$(document).ready(function() {
-
-	if (isTodayTuesday()) {
-		$('body').attr('data-isTuesday','true');
-		$('.answer-text').text('Yep.');
-
-	} else {
-
-		$('body').attr('data-isTuesday','false');
-		$('.answer-text').text('Nope.');
-
+function changeFavicon(src) {
+	// if we already have a favicon attached to the DOM, remove it
+	var favicons = $("link[rel='shortcut icon']");
+	if(favicons.length >= 1) {
+		favicons.each(function(i) {
+			favicons[i].remove();
+		});
 	}
-});
+
+	// plop in our new favicon
+	var link = document.createElement('link');
+	link.rel = 'shortcut icon';
+	link.href = src;
+	document.head.appendChild(link);
+}
 
